15fb6c6ef9dc63e1838d3baacf8693d3d87f15ce
david <davidmadrosan97@hotmail.com>
2021-04-13 18:21:11 +0200

    Prueba david

--- app/src/main/java/com/dam/lol/MainActivity.java
+++ app/src/main/java/com/dam/lol/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     // Prueba Chazo
+    // Prueba David
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
