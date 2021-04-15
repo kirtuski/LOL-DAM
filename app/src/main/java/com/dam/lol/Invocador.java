104f04097723d6d5a6af644e892bfbda8a3f8ed0
david <davidmadrosan97@hotmail.com>
2021-04-15 16:34:22 +0200

    Añadidas funciones get a la clase Invocador.java

--- app/src/main/java/com/dam/lol/Invocador.java
+++ app/src/main/java/com/dam/lol/Invocador.java
@@ -1,8 +1,29 @@
 package com.dam.lol;
 
 public class Invocador {
+    private String id;
     private String puuid;
     private String name;
     private int profileIconId;
     private int summonerLevel;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPuuid() {
+        return puuid;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getProfileIconId(){
+        return profileIconId;
+    }
+
+    public int getSummonerLevel(){
+        return summonerLevel;
+    }
 }
